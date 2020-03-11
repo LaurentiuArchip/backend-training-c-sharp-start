cb6fae857e759f2221f1500ab3f2d2e731287f5d
Laurentiu Archip <larchip@scottlogic.com>
2020-03-11 13:49:59 +0000

    update Startup.cs

--- src/ScottLogic.Internal.Training/WebApplication1/Startup.cs
+++ src/ScottLogic.Internal.Training/WebApplication1/Startup.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace ScottLogic.Internal.Training.Api
+namespace WebApplication1
 {
     public class Startup
     {
@@ -22,7 +29,7 @@ namespace ScottLogic.Internal.Training.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {

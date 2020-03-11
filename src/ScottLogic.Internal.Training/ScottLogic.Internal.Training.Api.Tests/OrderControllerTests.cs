b0a51558b1efb4a2cf609f2103547e79094b5d0b
Laurentiu Archip <larchip@scottlogic.com>
2020-03-11 09:26:52 +0000

    add empty OrderController class and a test class for it

---
+++ src/ScottLogic.Internal.Training/ScottLogic.Internal.Training.Api.Tests/OrderControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottLogic.Internal.Training.Api.Tests
+{
+    class OrderControllerTests
+    {
+    }
+}
---
+++ src/ScottLogic.Internal.Training/ScottLogic.Internal.Training.Api/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing;
+
+namespace ScottLogic.Internal.Training.Api.Controllers
+{
+    public class OrderController
+    {
+        public ActionResult Get()
+        {
+            var currentMatcher = new OrderMatcher();
+            return currentMatcher.ExistingOrders;
+        }
+    }
+}

1aa93f9f38a01765659554dbb531394135e44264
Laurentiu Archip <larchip@scottlogic.com>
2020-03-17 11:41:17 +0000

    add UserController structure, no code

---
+++ src/ScottLogic.Internal.Training/ScottLogic.Internal.Training.Api/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScottLogic.Internal.Training.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // POST: api/Users
+        [HttpPost]
+        public IActionResult Post([FromBody] UserModel user)
+        {
+            if (user.Username != null)
+            {
+                // User already exists
+
+                // New user, added to the database
+                return Ok();
+            }
+            else
+            {
+                // Wrong user data
+                return BadRequest();
+            }
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

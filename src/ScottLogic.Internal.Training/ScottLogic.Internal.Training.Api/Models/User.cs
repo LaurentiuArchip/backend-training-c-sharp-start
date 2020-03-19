19b6f4667cee00fa7610b5cfcfdc0917c80c2c49
Laurentiu Archip <larchip@scottlogic.com>
2020-03-19 10:29:20 +0000

    update user Id to be AccountNumber

--- src/ScottLogic.Internal.Training/ScottLogic.Internal.Training.Api/Models/User.cs
+++ src/ScottLogic.Internal.Training/ScottLogic.Internal.Training.Api/Models/User.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ScottLogic.Internal.Training.Api
 {
     public class User
     {
         [Key]
-        public int Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AccountNumber { get; set; }
         [Required]
         public string Username { get; set; }
         [Required]
